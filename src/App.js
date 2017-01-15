1f4769b8cc0e6b03b992f6b44b7b716f148b6202
Bill Baker <bill.baker@spensatech.com>
2017-01-15 12:47:45 -0500

    as functions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,86 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import list from './data.js';
+
+const lcMatch = (q, s) => s.toLowerCase().indexOf(q.toLowerCase()) >= 0;
+
+const isSearched = (query) => (item) => 
+      !query || lcMatch(query, item.title) || lcMatch(query, item.author);
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            list, // same meaning as 'list: list,'
+            query: '',
+        };
+        this.onSearchChange = this.onSearchChange.bind(this); // because js
+    }
+
+    onSearchChange(event) {
+        this.setState({ query: event.target.value });
+    }
+    
+    render() {
+        const hello = 'Welcome to _ Client';
+        const { list, query } = this.state;
+        return (
+            <AppPage greet={hello}>
+                <Search value={query} onChange={this.onSearchChange}>
+                    Search
+                </Search>
+                <Table list={list} pattern={query} />
+            </AppPage>
+        );
+    }
 }
+
+const Search = ({ value, onChange, children }) =>
+    <div className="search">
+        <form>
+            {children}
+            <input type="text"
+                   value={value} // doesn't seem necessary?
+                   onChange={onChange} />
+        </form>
+    </div>
+
+const Table = ({ list, pattern }) =>
+    <div className="results">
+        { list.filter(isSearched(pattern)).map((item) =>
+            <div key={item.objectID}>
+                <span><a href={item.url}>{item.title}</a></span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+            </div>
+        )}
+    </div>
+
+const AppPage = ({greet, children}) =>
+    <div className="App">
+        <AppHeader hello={greet} />
+            {children}
+        <AppFooter />
+    </div>
+
+function AppHeader({hello}) {
+    let even = true;
+//        function tick() {
+//            even = !even;
+//        }
+    return(
+        <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>{hello.replace("_", even ? "HN" : "NH")}</h2>
+        </div>
+    );
+}
+
+const AppFooter = () =>
+    <p className="App-intro">
+        To start, edit <code>src/App.js</code> and save to reload.
+    </p>
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,36 @@
+const list = [
+    {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walker',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 3,
+        points: 5,
+        objectID: 1,
+    },
+    {
+        title: 'Acting',
+        url: 'https://nathoni.us',
+        author: 'Nathan Alan Davis',
+        num_comments: 4,
+        points: 8,
+        objectID: 3,
+    },
+    {
+        title: 'Cute cat',
+        url: 'https://kitties.meow/cutest.jpg',
+        author: 'Nyanternet',
+        num_comments: 7302,
+        points: 851,
+        objectID: 2,
+    }
+];
+
+export default list;
